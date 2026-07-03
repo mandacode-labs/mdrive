f8c5925cd110e1d4cb4442e421e331ba457a773c
sauce-git <sauce@mandacode.com>
2026-07-03 18:03:29 +0900

    refactor: move txmanager to entx

--- internal/entx/manager.go
+++ internal/entx/manager.go
@@ -1,22 +1,24 @@
-package txmanager
+package entx
 
 import (
 	"context"
 	"github.com/mandacode-labs/mdrive/ent"
 )
 
-type entTxManager struct {
+type txKey struct{}
+
+type TxManager struct {
 	client *ent.Client
 }
 
 // WithTx implements [TxManager].
-func (e *entTxManager) WithTx(ctx context.Context, fn func(ctx context.Context) error) error {
+func (t *TxManager) WithTx(ctx context.Context, fn func(ctx context.Context) error) error {
 	if _, ok := ctx.Value(txKey{}).(*ent.Tx); ok {
 		// Already in a transaction, just call the function with the existing context.
 		return fn(ctx)
 	}
 
-	tx, err := e.client.Tx(ctx)
+	tx, err := t.client.Tx(ctx)
 	if err != nil {
 		return err
 	}
@@ -34,13 +36,13 @@ func (e *entTxManager) WithTx(ctx context.Context, fn func(ctx context.Context) 
 	return tx.Commit()
 }
 
-func NewEntTxManager(client *ent.Client) TxManager {
-	return &entTxManager{client: client}
+func NewTxManager(client *ent.Client) *TxManager {
+	return &TxManager{client: client}
 }
 
-// EntTxFromContext retrieves the ent.Tx from the context, if present.
+// FromContext retrieves the ent.Tx from the context, if present.
 // If the context does not contain an ent.Tx, it returns nil and false.
-func EntTxFromContext(ctx context.Context) (*ent.Tx, bool) {
+func FromContext(ctx context.Context) (*ent.Tx, bool) {
 	tx, ok := ctx.Value(txKey{}).(*ent.Tx)
 	return tx, ok
 }
